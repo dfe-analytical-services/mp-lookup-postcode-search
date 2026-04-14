3f3af1a81d876f10243efecb56138b2b3900716c
ROY <Ethan.ROY@EDUCATION.GOV.UK>
2026-04-14 14:19:18 +0100

    Move join into function

--- R/ui_panels/mp_lookup.R
+++ R/ui_panels/mp_lookup.R
@@ -1,11 +1,16 @@
 # Function to generate MP information table -------------------------------
 
-pconsearch <- dplyr::left_join(postcode_data, mp_data)
-
 mp_data_finder <- function(chosen_postcode) {
-  data <- pconsearch %>%
+  data <- postcode_data %>%
     dplyr::filter(Postcode == paste(chosen_postcode)) %>%
-    dplyr::rename(Party = party_text, Name = display_as, "Full Title" = full_title, "Member Email" = member_email, Constituency = pcon_name) %>%
+    dplyr::left_join(mp_data, by = "pcon_code") %>%
+    dplyr::rename(
+      Party = party_text,
+      Name = display_as,
+      "Full Title" = full_title,
+      "Member Email" = member_email,
+      Constituency = pcon_name
+    ) %>%
     select(-pcon_code, -member_id) %>%
     return(data)
 }
